9e1de4db49612985f47e13bc10d36e2de995c58c
Misha Doronin <misha@doronin.org>
2012-06-04 19:06:20 +0200

    Call superclass constructor

--- src/main/java/org/vamdc/validator/gui/settings/SettingField.java
+++ src/main/java/org/vamdc/validator/gui/settings/SettingField.java
@@ -4,13 +4,14 @@ import javax.swing.JTextField;
 
 import org.vamdc.validator.Setting;
 
-public class SettingField extends JTextField{
+public class SettingField extends JTextField implements SettingControl{
 
 	private static final long serialVersionUID = -9021148873833748715L;
 	
 	private final Setting option;
 	
 	public SettingField(Setting option){
+		super();
 		this.option = option;
 		load();
 	}
