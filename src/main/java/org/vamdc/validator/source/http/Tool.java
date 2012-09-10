aec6b44888912b584ac9597b53f0c533d45714c4
Misha Doronin <misha@doronin.org>
2012-09-10 14:55:56 +0200

    Add forgotten file

---
+++ src/main/java/org/vamdc/validator/source/http/Tool.java
@@ -0,0 +1,45 @@
+package org.vamdc.validator.source.http;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.vamdc.validator.Setting;
+import org.vamdc.validator.source.XSAMSSourceException;
+
+import com.sun.jersey.api.client.Client;
+
+public class Tool {
+
+	static Client getJerseyClient() {
+		Client client = Client.create();
+		client.setConnectTimeout(Setting.HTTP_CONNECT_TIMEOUT.getInt());
+		client.setReadTimeout(Setting.HTTP_DATA_TIMEOUT.getInt());
+		return client;
+	}
+	
+	static URL getURL(String url) throws XSAMSSourceException{
+		try {
+			return new URL(url);
+		} catch (MalformedURLException e) {
+			throw new XSAMSSourceException("Service base URL '"+url+"' is malformed \r\n");
+		}
+	}
+	
+	static HttpURLConnection openConnection(URL requestURL) throws XSAMSSourceException{
+		try {
+			HttpURLConnection conn = (HttpURLConnection) requestURL.openConnection();
+			conn.setConnectTimeout(Setting.HTTP_CONNECT_TIMEOUT.getInt());
+			conn.setReadTimeout(Setting.HTTP_DATA_TIMEOUT.getInt());
+			//Allow gzip encoding
+			if (Setting.UseGzip.getBool()){
+				conn.setRequestProperty("Accept-Encoding", "gzip");
+			}
+			return conn;
+		} catch (IOException e) {
+			throw new XSAMSSourceException(e.getMessage());
+		}
+	}
+	
+}
